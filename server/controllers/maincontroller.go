8338aa01508612de9b2e86c24f82264cdc888634
Cleiton Marques Souza <cleiton.marques.souza@gmail.com>
2015-08-14 17:37:56 -0300

    Version 2

--- server/controllers/maincontroller.go
+++ server/controllers/maincontroller.go
@@ -14,7 +14,7 @@ func (mc *MainController) GetHandler(responseWriter http.ResponseWriter, request
 }
 func (mc *MainController) GetVersionHandler(responseWriter http.ResponseWriter, request *http.Request, parameters map[string]string) {
 	responseWriter.Header().Add("Content-Type", "application/json")
-	fmt.Fprintf(responseWriter, `{"status":200, "version": "0.1.0.1"}`)
+	fmt.Fprintf(responseWriter, `{"status":200, "version": "0.1.0.2"}`)
 }
 
 func NewMainController() MainController {
